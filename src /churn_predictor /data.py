63b62a31f93d8660bd55c87d4ba1bab6ab61925b
Meriam Ben Fadhel <benfadhelmeriam@gmail.com>
2026-03-29 20:34:28 +0200

    add dataset loading and cleaning logic

--- src /churn_predictor /data.py
+++ src /churn_predictor /data.py
@@ -1,1 +1,14 @@
  
+from pathlib import Path
+import pandas as pd
+
+
+def load_data() -> pd.DataFrame:
+    project_root = Path(__file__).resolve().parents[2]
+    file_path = project_root / "data" / "telco_churn.csv"
+
+    df = pd.read_csv(file_path)
+    df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
+    df = df.dropna()
+
+    return df
